701c4273d94fa065dd7ccdeffa3f1c88fdacc2ae
haryfun <hary.vai@gmail.com>
2019-09-24 11:51:32 +0700

    fix something

--- src/Features/index.js
+++ src/Features/index.js
@@ -1,11 +1,9 @@
 /* PLOP_INJECT_IMPORT */
-import Example from './Example';
 import Home from './Home';
 import Auth from './Auth';
 
 export {
   /* PLOP_INJECT_EXPORT */
-	Example,
 	Home,
 	Auth,
 }
